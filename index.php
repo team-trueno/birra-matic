3a3cc2d568e3d5058016c5d990d118358e327c1e
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-07-08 12:29:34 -0300

    Creo estructura del Repo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    // Insertar acá todo el codigo que haga falta
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>

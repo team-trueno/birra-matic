538af46e6c34ad6e51306d349dbe45ccc06d7718
santiagofacchini <48771806+santiagofacchini@users.noreply.github.com>
2019-07-08 14:47:14 -0300

    Completo archivo contacto.php y agrego en funciones.php validarDatosContacto()

--- php/contacto.php
+++ php/contacto.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once('funciones.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +11,18 @@
     <title>Contacto</title>
 </head>
 <body>
-    <form>
-
+    <form class="contacto" method="post">
+        <input type="text" name="nombre" placeholder="Nombre">
+        <br>
+        <input type="text" name="apellido" placeholder="Apellido">
+        <br>
+        <input type="email" name="email" placeholder="Correo electrónico">
+        <br>
+        <input type="text" name="asunto" placeholder="Breve descripción">
+        <br>
+        <textarea name="mensaje" rows="10" cols="40">Por favor, escriba su mensaje aquí. Se enviará una copia del mismo a su casilla de correo electrónico.</textarea>
+        <br>
+        <button type="submit">Enviar</button>
     </form>
 </body>
 </html>
--- php/funciones.php
+++ php/funciones.php
@@ -1,5 +1,23 @@
 <?php
 
+// estos datos luego van a obtenerse de $_POST
+$datos = [
+    'nombre' => 'Santiago',
+    'apellido' => 'Facchini',
+    'email' => 'santiago.facchini@icloud.com',
+    'asunto' => 'Test',
+    'mensaje' => 'Mensaje'
+];
 
+var_dump($datos);
+
+function validarDatosContacto($datos) {
+    foreach ($datos as $key => $value) {
+        trim($value);
+        stripslashes($value);
+        htmlspecialchars($value);
+    }
+    return $datos;
+}
 
 ?>

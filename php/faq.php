b49ebbb8ac539b519a27ae66b42f694b3009fb1c
luisblafer <32635768+luisblafer@users.noreply.github.com>
2019-07-24 17:01:18 -0300

    Reestructuré la topología de tarjetas

--- php/faq.php
+++ php/faq.php
@@ -36,43 +36,86 @@ $faq = [
 <!DOCTYPE html>
 <html lang="es">
 <!-- TODO: Cargar archivo head.php -->
-<?php include("../config/head.php") ?>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<body class="text-center">
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>FAQ</title>
+</head>
+
+<body>
     <!-- SECTION: FAQ -->
     <section id="faq" class="first">
         <div class="container">
-            <h2>Preguntas frecuentes</h2>
-            <img src="../img/rueda_icono.png" alt="logo" width="150" height="150" class="mb-5">
-            <!-- TODO: crear funcion para obtener datos de las tablas *preguntas* y *respuestas* -->
-            <!-- Hago un foreach que recorre el array $faq -->
-            <?php foreach ($faq as $faqs) : ?>
-                <!-- Recorro el array y voy mostrando en el acordeón cada una de las preguntas y respuestas por id -->
-                <div class="accordion" id="accordionFAQ" style="border: 1px dashed gray;">
-                    <div class="card">
-                        <div class="card-header" id="heading<?= $faqs["id"] ?>">
-                            <h2 class="mb-0">
-                                <button class="btn btn-lg btn-warning" type="button" data-toggle="collapse" data-target="#collapse<?= $faqs["id"] ?>" aria-expanded="true" aria-controls="collapse<?= $faqs["id"] ?>">
-                                    <?= $faqs["pregunta"] ?>
-                                </button>
-                            </h2>
-                        </div>
+            <div class="row">
+                <div class="col">
+                    <h2>Preguntas frecuentes</h2>
+                    <img src="../img/rueda_icono.png" alt="logo" width="150" height="150" class="mb-5">
+                </div>
+            </div>
 
-                        <div id="collapse<?= $faqs["id"] ?>" class="collapse" aria-labelledby="heading<?= $faqs["id"] ?>" data-parent="#accordionFAQ" style="border-top: 1px dashed gray;">
-                            <div class="card-body">
-                                <?= $faqs["respuesta"] ?>
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <!-- TODO: crear funcion para obtener datos de las tablas *preguntas* y *respuestas* -->
+                            <!-- Hago un foreach que recorre el array $faq -->
+                            <!-- Recorro el array y voy mostrando en el acordeón cada una de las preguntas y respuestas por id -->
+                            <div class="accordion" id="accordionFAQ">
+                                <?php foreach ($faq as $faqs) : ?>
+                                <div class="card">
+                                    <div class="card-header" id="heading<?= $faqs["id"] ?>">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link text-reset" type="button" data-toggle="collapse" data-target="#collapse<?= $faqs["id"] ?>" aria-expanded="true" aria-controls="collapse<?= $faqs["id"] ?>">
+                                                <?= $faqs["pregunta"] ?>
+                                            </button>
+                                        </h5>
+                                    </div>
+
+                                    <div id="collapse<?= $faqs["id"] ?>" class="collapse" aria-labelledby="heading<?= $faqs["id"] ?>" data-parent="#accordionFAQ">
+                                        <div class="card-body">
+                                            <p class="card-text">
+                                                <?= $faqs["respuesta"] ?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php endforeach; ?>
                             </div>
                         </div>
                     </div>
                 </div>
-            <?php endforeach; ?>
+            </div>
+
+
+
+
         </div>
     </section>
-    <br>
 
-    <!-- TODO: Cargar archivo footer.php y librerias de jquery y jscript-->
-    <?php include("../config/footer.php") ?>
-    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 
 </body>
 
